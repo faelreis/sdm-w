b9dc099c2a957da1b9b4552de245143dbd342aa5
Rafael Reis Franco <rafaelreisfranco15@gmail.com>
2023-09-28 20:14:30 -0300

    Delete script file

--- 220923/script.js
+++
@@ -1,8 +0,0 @@
-async function getProduct(id) {
-    await fetch(`https://dummyjson.com/products/${id}`)
-    .then((response) => response.json())
-        .then((json) => {
-            console.log(json);
-            respjson.innerHTML = imprime(json);
-        });
-    }
